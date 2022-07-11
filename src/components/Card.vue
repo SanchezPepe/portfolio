77e5a76b29b9b338d5176c55eb7b030dbda6d255
sanchezpepe <sanchezpepe97@gmail.com>
2022-07-11 04:16:40 +0000

    flip cards

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div @mouseover="flip = true" @mouseleave="flip = false">
+    <transition name="fade">
+      <p class="absolute w-full h-full bg-blue-500" v-if="!flip">{{ front }}</p>
+    </transition>
+
+    <transition name="fade">
+      <p class="absolute w-full h-full bg-green-500" v-if="flip">
+        {{ back }}
+      </p>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  props: {
+    front: String,
+    back: String,
+  },
+  setup() {
+    const flip = ref(false);
+
+    return { flip };
+  },
+};
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="grid grid-cols-2">
-    <vue-flip
-      :v-model="true"
-      class="text-center border-4"
-      width="100%"
-      height="2.5rem"
-      :active-hover="true"
-    >
-      <template v-slot:front class="front"> ABC </template>
-      <template v-slot:back class="back"> TEST </template>
-    </vue-flip>
+    <Card front="FRONT" back="BACK"/>
+    <Card front="FRONT" back="BACK"/>
+    <Card front="FRONT" back="BACK"/>
+    <Card front="FRONT" back="BACK"/>
+
   </div>
 </template>
 
 <script>
-import VueFlip from "vue-flip";
+import Card from "./Card.vue"
 
 export default {
   components: {
-    "vue-flip": VueFlip,
-  },
+    Card,
+  }
 };
 </script>
 
