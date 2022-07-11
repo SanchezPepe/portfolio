404422f282b64180c4a6221c7cecaf2a9353523e
sanchezpepe <sanchezpepe97@gmail.com>
2022-07-11 00:59:54 +0000

    base components

---
+++ src/components/Info.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2 class="text-blue-400">Info Component</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2 class="text-green-400">Projects Component</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
