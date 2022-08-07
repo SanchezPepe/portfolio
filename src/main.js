fced5a6758d54231d99e693930c3cffdfa9f8494
sanchezpepe <sanchezpepe97@gmail.com>
2022-08-07 06:51:19 +0000

    added firebase

--- src/main.js
+++ src/main.js
@@ -1,26 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-
-
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faDev } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faDev } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 
-
-
-
-/* add icons to the library */
 library.add(faLinkedin, faGithub, faEnvelope, faDev)
-
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
