ce6237c80a59c72029878f0bc184b20e9734e646
sanchezpepe <sanchezpepe97@gmail.com>
2022-08-07 06:12:20 +0000

    added icons

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
-import Info from "./components/Info.vue"
-import Projects from "./components/Projects.vue"
+import Info from "./components/Info.vue";
+import Projects from "./components/Projects.vue";
 </script>
 
 <template>
-<div class="grid lg:grid-cols-2 h-screen dark:bg-stone-900 dark:text-white  font-serif ">
-  <Info/>
-  <Projects/>
-</div>
-
+  <div
+    class="grid lg:grid-cols-2 h-screen dark:bg-stone-900 dark:text-white font-serif"
+  >
+    <Info />
+    <Projects />
+  </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faDev } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
+
+/* add icons to the library */
+library.add(faLinkedin, faGithub, faEnvelope, faDev)
+
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
